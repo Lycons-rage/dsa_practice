42b5097941a32bb3f365da36288071913eea16e3
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-09 23:43:03 +0530

    Time: 0 ms (100%), Space: 8.7 MB (17.25%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,15 +1,32 @@
-// this is memoization
+// this is memoization top - down
+
+// class Solution {
+// public:
+//     int climbStairs(int n) {
+//         vector<int> dp(n+1, -1);
+//         return climbing(n, dp);
+//     }
+
+//     int climbing(int n, vector<int> &dp){
+//         if (n <= 1) return 1;
+//         if (dp[n] != -1) return dp[n];
+//         return dp[n] = climbing(n-1, dp) + climbing(n-2, dp);
+//     }
+// };
+
+
+// this is tabulation bottom - up
 
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1, -1);
-        return climbing(n, dp);
-    }
+        vector<int> dp(n+1);
 
-    int climbing(int n, vector<int> &dp){
-        if (n <= 1) return 1;
-        if (dp[n] != -1) return dp[n];
-        return dp[n] = climbing(n-1, dp) + climbing(n-2, dp);
+        dp[0] = dp[1] = 1;
+        for (int i = 2; i <= n; i++){
+            dp[i] = dp[i-1] + dp[i-2];
+        }
+
+        return dp[n];
     }
 };
