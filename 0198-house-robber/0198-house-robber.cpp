7d8a772c50ca88024bc8b21111d47be967dee831
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-12 00:27:29 +0530

    Time: 0 ms (100%), Space: 10.1 MB (88.63%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -22,21 +22,42 @@
 
 // this is tabulation bottom - up approach
 
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int size = nums.size();
+//         vector<int> dp(size+1, -1);
+        
+//         dp[0] = nums[0];
+        
+//         for (int i = 1; i < size; i++){
+//             int take = nums[i];
+//             if (i > 1) take += dp[i-2];
+//             int notTake = dp[i-1];
+
+//             dp[i] = max(take, notTake);
+//         }
+//         return dp[size-1];
+//     }
+// };
+
+// this is space optimization
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int size = nums.size();
-        vector<int> dp(size+1, -1);
-        
-        dp[0] = nums[0];
+        int prev = nums[0], prev2 = 0;
         
         for (int i = 1; i < size; i++){
             int take = nums[i];
-            if (i > 1) take += dp[i-2];
-            int notTake = dp[i-1];
+            if (i > 1) take += prev2;
+            int notTake = prev;
 
-            dp[i] = max(take, notTake);
+            int current = max(take, notTake);
+            prev2 = prev;
+            prev = current;
         }
-        return dp[size-1];
+        return prev;
     }
 };
