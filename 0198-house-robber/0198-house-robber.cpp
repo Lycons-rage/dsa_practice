1c5780bb5341eaa2ea7488a172f72660b903344d
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-12 00:19:50 +0530

    Time: 3 ms (6.04%), Space: 10.5 MB (61.26%) - LeetHub

--- 0198-house-robber/0198-house-robber.cpp
+++ 0198-house-robber/0198-house-robber.cpp
@@ -1,21 +1,42 @@
 // this is memoization top - down approach
 
+// class Solution {
+// public:
+//     int rob(vector<int>& nums) {
+//         int size = nums.size();
+//         vector<int> dp(size+1, -1);
+//         return memoization(nums, size-1, dp);
+//     }
+
+//     int memoization(vector<int> &nums, int index, vector<int> &dp){
+//         if (index == 0) return nums[index];
+//         if (index < 0) return 0;
+
+//         if (dp[index] != -1) return dp[index];
+//         int pick = nums[index] + memoization(nums, index-2, dp);
+//         int notPick = 0 + memoization(nums, index-1, dp);
+
+//         return dp[index] = max(pick, notPick);
+//     }
+// };
+
+// this is tabulation bottom - up approach
+
 class Solution {
 public:
     int rob(vector<int>& nums) {
         int size = nums.size();
         vector<int> dp(size+1, -1);
-        return memoization(nums, size-1, dp);
-    }
+        
+        dp[0] = nums[0];
+        
+        for (int i = 1; i < size; i++){
+            int take = nums[i];
+            if (i > 1) take += dp[i-2];
+            int notTake = dp[i-1];
 
-    int memoization(vector<int> &nums, int index, vector<int> &dp){
-        if (index == 0) return nums[index];
-        if (index < 0) return 0;
-
-        if (dp[index] != -1) return dp[index];
-        int pick = nums[index] + memoization(nums, index-2, dp);
-        int notPick = 0 + memoization(nums, index-1, dp);
-
-        return dp[index] = max(pick, notPick);
+            dp[i] = max(take, notTake);
+        }
+        return dp[size-1];
     }
 };
