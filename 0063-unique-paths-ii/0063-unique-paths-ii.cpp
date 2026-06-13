9a2b4b90fc6d434b42ae7a9553f9eeaefcb7b7c8
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-14 01:42:14 +0530

    Time: 0 ms (100%), Space: 12 MB (30.01%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -19,3 +19,24 @@ public:
         return dp[i][j] = up + left;
     }
 };
+
+// this is tabulation (tabulation has error, figure out by dry run)
+// class Solution {
+// public:
+//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+//         int row = obstacleGrid.size();
+//         int col = obstacleGrid[0].size();
+//         vector<vector<int>> dp(row+1, vector<int>(col+1, 0));
+//         dp[0][0] = 1;
+//         for (int i = 1; i < row; i++){
+//             for (int j = 1; j < col; j++){
+//                 if (obstacleGrid[i][j] != 1){
+//                     int up = dp[i-1][j];
+//                     int left = dp[i][j-1];
+//                     dp[i][j] = up + left;
+//                 }
+//             }
+//         }
+//         return dp[row-1][col-1];
+//     }
+// };
