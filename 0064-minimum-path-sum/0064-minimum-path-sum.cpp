77cff027483a23808087dbf234a0a0ec143e4145
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-15 02:31:38 +0530

    Time: 0 ms (100%), Space: 16 MB (73.4%) - LeetHub

--- 0064-minimum-path-sum/0064-minimum-path-sum.cpp
+++ 0064-minimum-path-sum/0064-minimum-path-sum.cpp
@@ -23,22 +23,52 @@
 // };
 
 // this is tabulation
+// class Solution {
+// public:
+//     int minPathSum(vector<vector<int>>& grid) {
+//         int row = grid.size();
+//         int col = grid[0].size();
+//         vector<vector<int>> dp(row, vector<int>(col, -1));
+
+//         dp[0][0] = grid[0][0];
+//         for (int i = 0; i < row; i++){
+//             for (int j = 0; j < col; j++){
+//                 int up = INT_MAX, left = INT_MAX;
+//                 if (i > 0) up = grid[i][j] + dp[i-1][j];
+//                 if (j > 0) left = grid[i][j] + dp[i][j-1];
+//                 if (i > 0 || j > 0) dp[i][j] = min(up, left);
+//             }
+//         }
+//         return dp[row-1][col-1];
+//     }
+// };
+
+// this is space optimization
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
         int row = grid.size();
         int col = grid[0].size();
-        vector<vector<int>> dp(row, vector<int>(col, -1));
+        vector<int> prev(col, 0);
 
-        dp[0][0] = grid[0][0];
         for (int i = 0; i < row; i++){
+            vector<int> curr(col, 0);
             for (int j = 0; j < col; j++){
-                int up = INT_MAX, left = INT_MAX;
-                if (i > 0) up = grid[i][j] + dp[i-1][j];
-                if (j > 0) left = grid[i][j] + dp[i][j-1];
-                if (i > 0 || j > 0) dp[i][j] = min(up, left);
+                if (i==0 && j==0) curr[j] = grid[i][j];
+                else{
+                    int up = grid[i][j];
+                    if (i > 0) up += prev[j];
+                    else up += 1e9;
+
+                    int left = grid[i][j];
+                    if (j > 0) left += curr[j-1];
+                    else left += 1e9;
+
+                    curr[j] = min(up, left);
+                }
             }
+            prev = curr;
         }
-        return dp[row-1][col-1];
+        return prev[col-1];
     }
 };
