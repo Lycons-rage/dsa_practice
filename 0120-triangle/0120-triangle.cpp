6f563088cd87615d8a3e9dd7a56a74b341341b6d
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-17 02:16:03 +0530

    Time: 0 ms (100%), Space: 13.3 MB (41.82%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -17,23 +17,49 @@
 // };
 
 // this is tabulation
+
+// class Solution {
+// public:
+//     int minimumTotal(vector<vector<int>>& triangle) {
+//         int n = triangle.size();
+//         vector<vector<int>> dp(n, vector<int>(n, -1e9));
+
+//         for (int j = 0; j < n; j++){
+//             dp[n-1][j] = triangle[n-1][j];
+//         }
+
+//         for (int i = n-2; i >= 0; i--){
+//             for (int j = i; j >= 0; j--){
+//                 int down = triangle[i][j] + dp[i+1][j];
+//                 int diagonal = triangle[i][j] + dp[i+1][j+1];
+//                 dp[i][j] = min(down, diagonal);
+//             }
+//         }
+//         return dp[0][0];
+//     }
+// };
+
+// this is space optimization
+
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n = triangle.size();
-        vector<vector<int>> dp(n, vector<int>(n, -1e9));
+        vector<int> prev(n, -1e9);
 
         for (int j = 0; j < n; j++){
-            dp[n-1][j] = triangle[n-1][j];
+            prev[j] = triangle[n-1][j];   // this sorts the base case
         }
 
         for (int i = n-2; i >= 0; i--){
+            vector<int> current(i+1, -1e9);
             for (int j = i; j >= 0; j--){
-                int down = triangle[i][j] + dp[i+1][j];
-                int diagonal = triangle[i][j] + dp[i+1][j+1];
-                dp[i][j] = min(down, diagonal);
+                int down = triangle[i][j] + prev[j];
+                int diagonal = triangle[i][j] + prev[j+1];
+                current[j] = min(down, diagonal);
             }
+            prev = current;
         }
-        return dp[0][0];
+        return prev[0];
     }
 };
