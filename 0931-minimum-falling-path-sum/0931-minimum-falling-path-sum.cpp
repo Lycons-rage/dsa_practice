0f266177181db708014f7dfb947877312317e247
Chinmaya Tewari <51325697+Lycons-rage@users.noreply.github.com>
2026-06-17 18:00:07 +0530

    Time: 1 ms (46.19%), Space: 15.2 MB (15.57%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.cpp
@@ -1,27 +1,62 @@
-// this is memoization
+// this is memoization top - down approach 
+
+// class Solution {
+// public:
+//     int minFallingPathSum(vector<vector<int>>& matrix) {
+//         int n = matrix.size();
+//         vector<int> low(n, -1);
+//         vector<vector<int>> dp(n, vector<int>(n, 1e9));
+//         for (int i = 0; i < n; i++){
+//             low[i] = findPath(n-1, i, n, matrix, dp);
+//         }
+
+//         auto min_path = min_element(low.begin(), low.end());
+//         return *min_path;
+//     }
+
+//     int findPath(int i, int j, int size, vector<vector<int>>& arr, vector<vector<int>>& dp){
+//         if (j < 0 || j > size-1) return 1e9;
+//         if (i == 0) return arr[i][j];
+//         if (dp[i][j] != 1e9) return dp[i][j];
+//         int up = arr[i][j] + findPath(i-1, j, size, arr, dp);
+//         int left = arr[i][j] + findPath(i-1, j-1, size, arr, dp);
+//         int right = arr[i][j] + findPath(i-1, j+1, size, arr, dp);
+
+//         return dp[i][j] = min({up, left, right});
+//     }
+// };
+
+// this is tabulation bottom - up approach
 
 class Solution {
 public:
     int minFallingPathSum(vector<vector<int>>& matrix) {
         int n = matrix.size();
-        vector<int> low(n, -1);
         vector<vector<int>> dp(n, vector<int>(n, 1e9));
         for (int i = 0; i < n; i++){
-            low[i] = findPath(n-1, i, n, matrix, dp);
+            dp[0][i] = matrix[0][i];
         }
 
-        auto min_path = min_element(low.begin(), low.end());
+        for (int i = 1; i < n; i++){
+            for (int j = 0; j < n; j++){
+                int up = matrix[i][j] + dp[i-1][j];
+                int left = 0;
+                if (j > 0){
+                    left = matrix[i][j] + dp[i-1][j-1];
+                } else {
+                    left = 1e9;
+                }
+                int right = 0;
+                if (j < n-1){
+                    right = matrix[i][j] + dp[i-1][j+1];
+                } else {
+                    right = 1e9;
+                }
+                dp[i][j] = min({up, left, right});
+            }
+        }
+
+        auto min_path = min_element(dp[n-1].begin(), dp[n-1].end());
         return *min_path;
-    }
-
-    int findPath(int i, int j, int size, vector<vector<int>>& arr, vector<vector<int>>& dp){
-        if (j < 0 || j > size-1) return 1e9;
-        if (i == 0) return arr[i][j];
-        if (dp[i][j] != 1e9) return dp[i][j];
-        int up = arr[i][j] + findPath(i-1, j, size, arr, dp);
-        int left = arr[i][j] + findPath(i-1, j-1, size, arr, dp);
-        int right = arr[i][j] + findPath(i-1, j+1, size, arr, dp);
-
-        return dp[i][j] = min({up, left, right});
     }
 };
